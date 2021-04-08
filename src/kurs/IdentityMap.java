037cb7a1266ea2992e5dabe70fe1e1bec18f7c43
YuriDyachenko <yuri.dyachenko@outlook.com>
2021-04-08 19:12:33 +0300

    hw done

---
+++ src/kurs/IdentityMap.java
@@ -0,0 +1,38 @@
+package kurs;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class IdentityMap {
+    private Map<Integer, Writer> writerMap = new HashMap<>();
+    private Map<Integer, Book> bookMap = new HashMap<>();
+
+    public Writer getWriterById(int id) {
+        if (writerMap.containsKey(id)) {
+            System.out.println("...получение писателя из кэша...");
+            return writerMap.get(id);
+        };
+        Writer writer = new WriterMapper().getById(id);
+        if (writer != null) putWriter(writer);
+        return writer;
+    }
+
+    public void putWriter(Writer writer) {
+        writerMap.put(writer.getId(), writer);
+    }
+
+    public Book getBookById(int id) {
+        if (bookMap.containsKey(id)) {
+            System.out.println("...получение книги из кэша...");
+            return bookMap.get(id);
+        };
+        Book book = new BookMapper().getById(id);
+        if (book != null) putBook(book);
+        return book;
+    }
+
+    public void putBook(Book book) {
+        bookMap.put(book.getId(), book);
+    }
+
+}
--- src/kurs/Main.java
+++ src/kurs/Main.java
@@ -1,7 +1,44 @@
 package kurs;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Main {
 
     public static void main(String[] args) {
+
+        AppContext app = AppContext.getInstance();
+        Connection connection = app.getConnection();
+        if (connection != null) {
+            try {
+                System.out.println("---------- Все писатели в базе ----------");
+                Statement statement = connection.createStatement();
+                ResultSet rs = statement.executeQuery("SELECT name FROM writers");
+                while (rs.next()){
+                    System.out.println(" " + rs.getString("name"));
+                }
+                rs.close();
+                System.out.println("---------- Все книги в базе ----------");
+                rs = statement.executeQuery("SELECT name FROM books");
+                while (rs.next()){
+                    System.out.println(" " + rs.getString("name"));
+                }
+                rs.close();
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+
+            System.out.println("---------- Получение писателя по id (два раза) ----------");
+            System.out.println(app.getIdentityMap().getWriterById(1));
+            System.out.println(app.getIdentityMap().getWriterById(1));
+
+            System.out.println("---------- Получение книги по id (два раза) ----------");
+            System.out.println(app.getIdentityMap().getBookById(2));
+            System.out.println(app.getIdentityMap().getBookById(2));
+        }
+
+        app.closeConnection();
     }
 }
